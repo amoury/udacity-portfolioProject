1d47b05a55fcc15dbd4b67a5fd508222dffea785
Amoury <ansar.memon@hotmail.com>
2016-09-05 14:40:19 +0400

    Complete Portfolio Website

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,10 @@
 module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-responsive-images');
+  	grunt.loadNpmTasks('grunt-contrib-clean');
+  	grunt.loadNpmTasks('grunt-contrib-copy');
+  	grunt.loadNpmTasks('grunt-mkdir');
 	
 
 	grunt.initConfig({
@@ -16,7 +20,52 @@ module.exports = function(grunt) {
 				
 			}
 		},
-		watch: {
+
+		/* Clear out the images directory if it exists */
+    	clean: {
+      		dev: {
+        		src: ['images'],
+      		},
+    	},
+
+		responsive_images: {
+      		dev: {
+        		options: {
+	          		engine: 'im',
+	          		sizes: [{
+            			width: 1600,
+            			quality: 30,
+            			suffix: '_large_2x'
+          			},
+          			{
+          				width: 800,
+          				quality: 30,
+          				suffix: '_small_2x'
+          			}]
+        		},
+
+        /* You don't need to change this part if you don't change the directory structure. */
+        		files: [{
+          			expand: true,
+          			src: ['*.{gif,jpg,jpeg,png}'],
+          			cwd: 'img/images_src/',
+          			dest: 'images/'
+        			}]
+      			}
+    	},
+
+   
+
+    /* Generate the images directory if it is missing */
+    	mkdir: {
+      		dev: {
+        		options: {
+          			create: ['images']
+        		},
+      		},
+    	},
+
+    	watch: {
   			css: {
     			files: ['src/sass/*.scss'],
     			tasks: ['sass'],
@@ -25,10 +74,15 @@ module.exports = function(grunt) {
     			}
   			}
 		}
+
+    	
 	});
 
 	grunt.registerTask('default',[
 		'sass',
+		'clean',
+		'responsive_images',
+		'mkdir',
 		'watch'
 		]);
 }
